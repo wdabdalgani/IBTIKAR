f6ef33849b9b45109d367c9661e9608ce6d0c13a
wdabdalgani <155243288+wdabdalgani@users.noreply.github.com>
2024-07-15 17:57:29 +0200

    Update main.js

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,7 +1,4 @@
-
-
-
-// declear all the varable 
+ // declear all the varable 
 // call the input form main page index.html
 // aft
 // let Num_fucture = document.getElementById('Num_fucture');
@@ -10,10 +7,7 @@ let count = document.getElementById('count');
 let prix_one = document.getElementById('prix_one');
 let data_fucture = document.getElementById('data_fucture');
 // btn onclick start the main functions 
-
 let submit = document.getElementById('submit');
-
-
 // array to save data 
 // in this array we well sava tha data in array and send the data 
 // to local storage and after that 
@@ -36,54 +30,58 @@ submit.onclick = function()
         prix_one:prix_one.value,
         data_fucture:data_fucture.value,
     }
-    datasale.push(newsale);
-    localStorage.setItem('product' , JSON.stringify(datasale));
-
-   showdata();
- 
+    if(Description.value == '' || count.value == '' ||  prix_one.value == '' ){
+        let msg = 'ألرجاء مل كل الحقول '; 
+        document.getElementById('msg').innerHTML = msg;
+    }else{
+        datasale.push(newsale);
+        localStorage.setItem('product' , JSON.stringify(datasale));
+    showdata();
+    Description.value = '';
+    count.value = '';
+    prix_one.value = '';
+    element.classList.remove('msg');
+    }
 }
-
 function showdata(){
-   let table = '';
-   for(let i = 0; i < datasale.length;i++)
+    let table = '';
+    y = 1; 
+    for(let i = 0; i < datasale.length;i++)
 {
     table += `
-           <tr>
-                <td>${i}</td>
-              
+        <tr>
+                <td>${y}</td>
                 <td> ${datasale[i].Description}</td>
                 <td>${datasale[i].count}</td>
                 <td>${datasale[i].prix_one}  </td>
                 <td>${datasale[i].data_fucture }     </td>
             </tr>
-   `
- 
+`
+    y +=1;
 }  
 let tablemain = '';
 let Myarray = [];
 let sum  = 0;
+x = 1;
 for(let i = 0; i < datasale.length;i++)
 {
-
-
+    // show the date in the main total  
     let total = datasale[i].count * datasale[i].prix_one;
     sum += total;
-    Myarray.push(sum);
-
+    Myarray.push(sum);  
     tablemain += `
-        <tr>
-             <td>${i}</td>
-             <td> ${datasale[i].Description}</td>
-             <td>${datasale[i].count}</td>
-             <td>${datasale[i].prix_one}  </td>
-             <td> ${total} </td>
-             
-         </tr>
+    <tr>
+            <td>${x}</td>
+            <td> ${datasale[i].Description}</td>
+            <td>${datasale[i].count}</td>
+            <td>${datasale[i].prix_one}  </td>
+            <td> ${total + ' F'} </td>
+            
+        </tr>
 `   
-
+    x +=1;
 let last = Myarray[Myarray.length-1];
-
-    document.getElementById('total_all').innerHTML = last;
+    document.getElementById('total_all').innerHTML = last + ' F';
     document.getElementById('total_all_montant').innerHTML = last;
 }  
 console.log(Myarray);
@@ -91,33 +89,31 @@ console.log(Myarray);
 document.getElementById('main_data_show').innerHTML = tablemain;      
 document.getElementById('tbody').innerHTML = table;      
 let btndelat = document.getElementById('deleteall');
-
 if(datasale.length > 0){
     btndelat.innerHTML = ` 
     <button onclick='deleteall()' id="deleteall1">delete All </button>
     `
-
 }
 else{
     btndelat.innerHTML = '';
 }
 }
-
-
 function deleteall(){
     localStorage.clear();
     datasale.splice(0);
     showdata();
 }  
-
 // maindata() 
 function maindata(){
     let C_name = document.getElementById('C_name').value;
     let C_num = document.getElementById('C_num').value;
     let C_data = document.getElementById('C_data').value;
-    
     document.getElementById('name_claine').innerText = C_name;
     document.getElementById('num_facture_a').innerText = C_num;
     document.getElementById('data_fact').innerText = C_data;
-
+}
+function NewFacture(){
+    localStorage.clear();
+    datasale.splice(0);
+    showdata();
 }
